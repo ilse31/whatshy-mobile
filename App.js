a49beaaaf38de4425de6b503e74c2da7c41e0021
ilse31 <ilhamprazsetyo33@gmail.com>
2023-01-27 15:01:04 +0700

    setup tailwind

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
 import { LogBox } from "react-native";
+import { View, Text } from "react-native";
+import Login from "./src/pages/Login";
 
 export default function App() {
   LogBox.ignoreLogs(["Warning: ..."]);
   return (
-    <View className='flex-1 items-center justify-center bg-slate-900'>
-      <Text className='text-white'>
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style='auto' />
-    </View>
+    <>
+      <Login />
+      <StatusBar style='light' />
+    </>
   );
 }
---
+++ src/pages/Login.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import { SafeAreaView, Text } from 'react-native'
+
+const Login = () =>
+{
+    return (
+        <SafeAreaView className='bg-gray-900 flex-1 justify-center items-center'
+        >
+            <Text className='text-white text-2xl'>Login</Text>
+        </SafeAreaView>
+    )
+}
+
+export default Login
