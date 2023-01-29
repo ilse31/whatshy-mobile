716d3bef7b0d4a01dc334d222e5de5870504fb48
ilse31 <ilhamprazsetyo33@gmail.com>
2023-01-29 11:55:22 +0700

    add svg and icon

---
+++ assets/svg/index.js
@@ -0,0 +1,11 @@
+import Logo from "./icon.png";
+import Banner from "./rafiki.png";
+import Profile from "./profile_bg.svg";
+
+const svg = {
+  Logo,
+  Banner,
+  Profile,
+};
+
+export default svg;
