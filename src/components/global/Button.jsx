94a3448c39d2d3aa6cbec0fa4a8cf3dd3c6fa866
ilse31 <ilhamprazsetyo33@gmail.com>
2023-01-29 11:55:31 +0700

    add button

---
+++ src/components/global/Button.jsx
@@ -0,0 +1,19 @@
+import { View, Text, TouchableOpacity, Pressable } from 'react-native'
+import React from 'react'
+
+const Button = ( {
+    onPress,
+    title,
+    accessibilityLabel,
+    disabled,
+    w = 52,
+} ) =>
+{
+    return (
+        <TouchableOpacity activeOpacity={ 1 } onPressIn={ onPress } accessibilityLabel={ accessibilityLabel } className={ ` w-${ w } ${ disabled ? "bg-gray-200" : "hover:bg-emerald-700 bg-emerald-500  active:bg-emerald-600" } h-12 rounded-full items-center justify-center active:ring active:ring-offset-2` } disabled={ disabled }>
+            <Text className=" text-white font-PoppinsSemiBold capitalize">{ title }</Text>
+        </TouchableOpacity>
+    )
+}
+
+export default Button
