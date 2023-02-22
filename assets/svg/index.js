9d4f334b23b0ddba2ae1d418cd2e4e86124a9546
ilse31 <ilhamprazsetyo33@gmail.com>
2023-02-23 00:15:58 +0700

    fiks profile pages

--- assets/svg/index.js
+++ assets/svg/index.js
@@ -1,11 +1,15 @@
 import Logo from "./icon.png";
 import Banner from "./rafiki.png";
 import Profile from "./profile_bg.svg";
+import circleIL from "./circleIL.png";
+import fotoProfile from "./Profile.png";
 
 const svg = {
   Logo,
   Banner,
   Profile,
+  circleIL,
+  fotoProfile,
 };
 
 export default svg;
--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,28 +1,60 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 import Mainlayouts from '../layouts/Mainlayouts'
 import AsyncStorage from '@react-native-async-storage/async-storage'
-import { TouchableOpacity } from 'react-native-gesture-handler'
 import { useNavigation } from '@react-navigation/native'
+import svg from '../../assets/svg'
+import { AntDesign } from '@expo/vector-icons';
+import { useEffect } from 'react'
+import { useState } from 'react'
 
 const Account = () =>
 {
     const navigate = useNavigation()
-    const clearAsyncStorage = async () =>
+    const [ users, setUsers ] = useState()
+    useEffect( () =>
     {
-        await AsyncStorage.clear();
-        navigate.navigate( "login" )
-    }
+        AsyncStorage.getItem( "users" ).then( res =>
+        {
+            setUsers( JSON.parse( res ) )
+        } )
+    }, [] )
+
 
     return (
         <Mainlayouts>
-            <View>
-                <TouchableOpacity onPress={clearAsyncStorage}>
-                    <Text>Logout</Text>
-                </TouchableOpacity>
+            <Image
+                style={ { position: "absolute", alignSelf: "center" } }
+                source={ svg.circleIL }
+            />
+            <View className="pt-10 pb-5 px-5">
+                <View className="justify-between flex-row items-center">
+                    <Text className="text-2xl font-bold text-center text-white font-PoppinsBold">Account</Text>
+                    <AntDesign name="logout" size={ 24 } color="white" />
+                </View>
+                <View className="items-center mt-5">
+                    <Image source={ svg.fotoProfile } />
+                    <Text className="text-2xl font-PoppinsBold mt-5 capitalize">{ users?.username }</Text>
+                </View>
+                <View
+                    style={ {
+                        backgroundColor: "rgba(0, 215, 185, 0.4)",
+                        borderRadius: 16,
+                        width: "100%",
+                    } }>
+                    <View style={ { padding: 20 } }>
+                        <Text
+                            className="text-sm font-PoppinsMedium">
+                            Email
+                        </Text>
+                        <Text
+                            className="text-sm font-PoppinsMedium">
+                            { users?.email }
+                        </Text>
+                    </View>
+                </View>
             </View>
-
-        </Mainlayouts>
+        </Mainlayouts >
     )
 }
 
